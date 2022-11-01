efa5a97589b7596f9b0ad7461e5c19873dca7217
Christian Manzaraz <magnusmanz@gmial.com>
2022-10-31 22:25:28 -0300

    Building the View: arranging UI Elements

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var signLabel: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+    
+    @IBOutlet var rockButton: UIButton!
+    @IBOutlet var paperButton: UIButton!
+    @IBOutlet var scissorsButton: UIButton!
+    
+    
+    @IBOutlet var playAgainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rockChoosen(_ sender: Any) {
+    }
+    
+    @IBAction func paperChoosen(_ sender: Any) {
+    }
+    
+    @IBAction func scissorsChoosen(_ sender: Any) {
+    }
+    
+    @IBAction func playAgainChoosen(_ sender: Any) {
+    }
 }
 
